15f9fc549e21f3eacbdd649e4197e3aba3d42096
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-08-08 21:32:02 +0300

    Update project settings

--- SwiftCards/SwiftCardsApp.swift
+++ SwiftCards/SwiftCardsApp.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftCardsApp: App {
+    init() { FirebaseApp.configure() }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CardListView(cardListViewModel: CardListViewModel())
         }
     }
 }
