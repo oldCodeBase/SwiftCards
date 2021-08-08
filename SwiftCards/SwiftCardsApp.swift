ea52807afa9abb646e5ff1fb0ddc7eab0654cd96
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-08-08 15:23:40 +0300

    Initial Commit

---
+++ SwiftCards/SwiftCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCardsApp.swift
+//  SwiftCards
+//
+//  Created by Ibragim Akaev on 08/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
